4b75de306da3425ea3eaa8c863e087469d852190
buunguyen <buunguyen@localhost>
2009-12-27 05:18:03 +0000

    - Add [assembly: AllowPartiallyTrustedCallers]

--- Fasterflect/Fasterflect/Properties/AssemblyInfo.cs
+++ Fasterflect/Fasterflect/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -15,6 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
+[assembly: AllowPartiallyTrustedCallers]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
