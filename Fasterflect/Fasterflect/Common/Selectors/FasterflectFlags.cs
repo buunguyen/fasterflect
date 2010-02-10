1f0be84a663b316c0f95b8138f0efc2f7330558a
mertner <mertner@localhost>
2010-02-10 17:42:22 +0000

    Added suggestion for general query criteria subsystem (aka reworking the Flags enum).

---
+++ Fasterflect/Fasterflect/Common/Selectors/FasterflectFlags.cs
@@ -0,0 +1,54 @@
+﻿#region License
+// Copyright 2010 Buu Nguyen, Morten Mertner
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+// 
+// http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+// 
+// The latest version of this file can be found at http://fasterflect.codeplex.com/
+#endregion
+
+using System;
+
+namespace Fasterflect.Selectors
+{
+	[Flags]
+	public enum FasterflectFlags : long
+	{
+		// BindingFlags
+	    None                 = 0, // BindingFlags.Default
+	    IgnoreCase           = 0x1,
+	    DeclaredOnly         = 0x2,
+	    Instance             = 0x4,
+	    Static               = 0x8,
+	    Public               = 0x10,
+	    NonPublic            = 0x20,
+	    //FlattenHierarchy     = 0x40,
+		//InvokeMethod         = 0x100,
+		//CreateInstance       = 0x200,
+		//GetField             = 0x400,
+		//SetField             = 0x800,
+		//GetProperty          = 0x1000,
+		//SetProperty          = 0x2000,
+		//PutDispProperty      = 0x4000,
+		//PutRefDispProperty   = 0x8000,
+	    ExactBinding         = 0x10000,
+	    SuppressChangeType   = 0x20000,
+	    OptionalParamBinding = 0x40000,
+	    IgnoreReturn         = 0x1000000,
+
+		// Fasterflect
+		PartialNameMatch     = 0x100000000,
+	    ExplicitNameMatch    = 0x200000000,
+	    ParameterMatch       = 0x400000000,
+	    ExactParameterMatch  = 0x800000000,
+	}
+}
---
+++ Fasterflect/Fasterflect/Common/Selectors/ParameterMatch.cs
@@ -0,0 +1,44 @@
+﻿#region License
+// Copyright 2010 Buu Nguyen, Morten Mertner
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+// 
+// http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+// 
+// The latest version of this file can be found at http://fasterflect.codeplex.com/
+#endregion
+
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Fasterflect.Selectors
+{
+	internal class ParameterMatch : IMethodSelector
+	{
+		#region Implementation of IMethodSelector
+		public bool IsMatch( MethodBase info, Flags flags, Type[] paramTypes, Type returnType )
+		{
+       		var methodParameterTypes = info.Parameters().Select( p => p.ParameterType ).ToList();
+			if( methodParameterTypes.Count != paramTypes.Length )
+				return false;
+			return !methodParameterTypes.Where( ( t, i ) => ! t.IsAssignableFrom( paramTypes[ i ] ) ).Any();
+		}
+		#endregion
+
+		#region Implementation of ISelector
+		public bool IsMatch( MemberInfo info, MemberTypes memberTypes, Flags flags, string name, Type[] paramTypes, Type returnType )
+		{
+			return IsMatch( info as MethodBase, flags, paramTypes, returnType );
+		}
+		#endregion
+	}
+}
