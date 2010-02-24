a95c169652b85f0e3aa21408063c2d0b20e70a6a
mertner <mertner@localhost>
2010-02-24 23:07:52 +0000

    Removed AllowPartiallyTrustedCallers attribute as I don't think its appropriate. This should also fix code coverage under CLR 4.

--- Fasterflect/Fasterflect/Properties/AssemblyInfo.cs
+++ Fasterflect/Fasterflect/Properties/AssemblyInfo.cs
@@ -17,7 +17,6 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
-[assembly: AllowPartiallyTrustedCallers]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
