fd957f18b7f602739eaaa992010ff76190231363
mnmr <mnmr@localhost>
2012-09-14 05:50:58 +0200

    Updated version to 2.1.1. Updated build file to use VS 2012 paths.

--- Fasterflect/SolutionInfo.cs
+++ Fasterflect/SolutionInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.1.0.*")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.1.*")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
